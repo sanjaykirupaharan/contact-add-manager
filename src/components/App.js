0076036abcc2e0c4a1469ca9ed663e71c9c2f010
Sanjay <sanjaykirupaharan21@gmail.com>
2022-11-16 16:21:30 +0530

    feat: add local storage storing facility

--- src/components/App.js
+++ src/components/App.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import './App.css';
 
 import Header from './Header';
@@ -6,13 +6,18 @@ import AddContact from './AddContact';
 import ContactList from './ContactList';
 
 function App() { 
+  const LOCAL_STORAGE_KEY = "contacts"
   const [contacts, setContacts] = useState([]);  
 
   const addContactHandler = (contact) => {
     console.log(contact);
     setContacts([...contacts, contact])
-
   };
+
+    useEffect (() => {
+      localStorage.setItem(LOCAL_STORAGE_KEY, JSON.stringify(contacts));
+    }, [contacts]);
+
   return (
     <div className='ui container'>
       <Header />
