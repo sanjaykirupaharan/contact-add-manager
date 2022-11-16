b3e53c18e4cde101c6fb9a2e88e958537eb65976
Sanjay <sanjaykirupaharan21@gmail.com>
2022-11-16 21:04:47 +0530

    feat: add delete function

--- src/components/ContactList.js
+++ src/components/ContactList.js
@@ -4,10 +4,12 @@ import ContactCard from './ContactCard';
 const ContactList = (props) => {
    console.log(props);
 
-   const deleteContactHandler 
+   const deleteContactHandler = (id) => {
+     props.getContactId(id);
+   } ;
 
    const renderContactList = props.contacts.map((contact)=> {
-       return  <ContactCard contact={contact} /> ;
+       return  <ContactCard contact={contact} clickHandler = {deleteContactHandler} key = {contact.id} /> ;
        
    })
    return(
